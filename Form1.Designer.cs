9d4183d613e3ea19c3d6ffec1d5477090485548d
Clancy <72635941+Clancy-yang@users.noreply.github.com>
2021-01-29 17:35:32 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace CNET_TEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.input = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 21);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(244, 98);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(12, 152);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(244, 21);
+            this.input.TabIndex = 1;
+            this.input.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(182, 196);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "发送";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 196);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "连接";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 196);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "退出";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 231);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "测试工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace CNET_TEST
+{
+    [StructLayoutAttribute(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)]
+    public struct DataHeader
+    {
+        public UInt16 flags;     // 标志位:0x01登录,0x02退出,0x03发送信息
+        public UInt16 data_len;  // 负载长度(小于1000)
+        
+        public DataHeader(UInt16 flags, UInt16 data_len)
+        {
+            this.flags = flags;
+            this.data_len = data_len;
+        }
+    };
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if(client != null)
+            {
+                try
+                {
+                    string s = input.Text;
+                    DataHeader dataHeader = new DataHeader(0x03, (UInt16)s.Length);
+                    byte[] header = StructToBytes(dataHeader);
+                    ShowMsg(dataHeader.flags + " " + dataHeader.data_len);
+                    byte[] data = Encoding.UTF8.GetBytes(input.Text);
+                    int len = header.Length + data.Length;
+                    byte[] buffer = new byte[len];
+                    //Buffer.BlockCopy(header, 0, buffer, 0, header.Length);
+                    //Buffer.BlockCopy(data, 0, buffer, header.Length, data.Length);
+                    buffer = header.Concat(data).ToArray();
+                    client.Send(buffer);
+                }
+                catch(Exception ex)
+                {
+                    ShowMsg(ex.Message);
+                }
+            }
+            else
+            {
+                ShowMsg("未连接!");
+            }
+        }
+
+        Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        Thread th;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(client == null)
+            {
+                client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            }
+            IPAddress ip = IPAddress.Parse("121.43.42.24");
+            IPEndPoint point = new IPEndPoint(ip, int.Parse("1234"));
+            try
+            {
+                client.Connect(point);
+                ShowMsg("连接成功");
+                DataHeader data = new DataHeader(0x01, 0);
+                byte[] buffer = StructToBytes(data);
+                client.Send(buffer);
+                ShowMsg("登录中..");
+                //连接成功后，就可以接收服务器发送的信息了
+                th = new Thread(new ThreadStart(delegate
+                {
+                    Control.CheckForIllegalCrossThreadCalls = false;//添加这一句即可
+                    ReceiveMsg();
+                }));
+                th.IsBackground = true;
+                th.Start();
+            }
+            catch (Exception ex)
+            {
+                ShowMsg("连接失败[" + ex.Message + "]");
+            }
+
+        }
+
+        //接收服务器的消息
+        void ReceiveMsg()
+        {
+            while (true)
+            {
+                if (client == null) break;
+                
+                try
+                {
+                    byte[] buffer = new byte[1024 * 1024];
+                    int n = client.Receive(buffer);
+                    UInt16 flag = BitConverter.ToUInt16(buffer, 0);
+                    UInt16 data_len = BitConverter.ToUInt16(buffer, 2);
+                    string data = Encoding.UTF8.GetString(buffer, 4, data_len);
+
+                    if(flag == 0x01 || flag == 0x03)
+                    {
+                        ShowMsg(client.RemoteEndPoint.ToString() + ":" + data);
+                    }
+                    else if (flag == 0x02)
+                    {
+                        ShowMsg(client.RemoteEndPoint.ToString() + ":" + data);
+                        client.Shutdown(SocketShutdown.Both);
+                        client.Close();
+                        client = null;
+                        Application.ExitThread();
+                        break;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    ShowMsg(ex.Message);
+                    break;
+                }
+            }
+            return; 
+        }
+
+
+        /// <summary>
+        /// 结构体转化成byte[]
+        /// </summary>
+        /// <param name="structure"></param>
+        /// <returns></returns>
+        public static Byte[] StructToBytes(Object structure)
+        {
+            Int32 size = Marshal.SizeOf(structure);
+            IntPtr buffer = Marshal.AllocHGlobal(size);
+            try
+            {
+                Marshal.StructureToPtr(structure, buffer, false);
+                Byte[] bytes = new Byte[size];
+                Marshal.Copy(buffer, bytes, 0, size);
+
+                return bytes;
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(buffer);
+            }
+        }
+        /// <summary>
+        /// byte[]转化成结构体
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="strcutType"></param>
+        /// <returns></returns>
+        public static Object BytesToStruct(Byte[] bytes, Type strcutType)
+        {
+            Int32 size = Marshal.SizeOf(strcutType);
+            IntPtr buffer = Marshal.AllocHGlobal(size);
+            try
+            {
+                Marshal.Copy(bytes, 0, buffer, size);
+
+                return Marshal.PtrToStructure(buffer, strcutType);
+            }
+            finally
+            {
+                Marshal.FreeHGlobal(buffer);
+            }
+        }
+        void ShowMsg(string msg)
+        {
+            textBox1.AppendText(msg + "\r\n");
+        }
+
+        public int IntToBitConverter(byte[] bytes)
+        {
+            int temp = BitConverter.ToInt32(bytes, 0);
+            return temp;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(client != null)
+            {
+                try
+                {
+                    DataHeader data = new DataHeader(0x02, 0);
+                    byte[] buffer = StructToBytes(data);
+                    client.Send(buffer);
+                    ShowMsg("退出中..");
+                }
+                catch (Exception ex)
+                {
+                    ShowMsg(ex.Message);
+                }
+            }
+            else
+            {
+                ShowMsg("未连接!");
+            }
+            
+        }
+    }
+
+}
